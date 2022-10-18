c60a654a1a561675ad904010f90accedf65a945c
Francisco <f.parrinha@fct.unl.pt>
2022-10-18 23:03:31 +0100

    Added swap function

--- src/RouteClass.java
+++ src/RouteClass.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.Random;
 
 public class RouteClass {
 
@@ -9,12 +10,12 @@ public class RouteClass {
 
     public RouteClass(String filter, DistanceMatrix dm){
         this.route = dm.getCities(filter);
-        Collections.shuffle(this.route);
+        swapRoute();
         setTotalRouteDistance(dm);
     }
     public RouteClass(RouteClass route, DistanceMatrix dm) {
         this.route = dm.getCities(dm.getInitials(route.getAllCities()));
-        Collections.shuffle(this.route);
+        swapRoute();
         setTotalRouteDistance(dm);
     }
 
@@ -39,12 +40,33 @@ public class RouteClass {
         return route.size();
     }
 
+
     public String getCity(int index) {
         return route.get(index);
     }
 
     public ArrayList<String> getAllCities(){
         return this.route;
+    }
+
+    public void swapRoute(){
+        Random rand = new Random();
+
+        // Get random indexes
+        int idx1 = rand.nextInt(route.size());
+        int idx2 = rand.nextInt(route.size());
+
+        // Makes sure that the second number is different than the first
+        while(idx2 == idx1){
+            idx2 = rand.nextInt(route.size());
+        }
+
+        // Swap positions
+        String aux = route.get(idx1);
+        route.set(idx1, route.get(idx2));
+        route.set(idx2, aux);
+
+        System.out.println("Hello");
     }
 
     public String toString(){
